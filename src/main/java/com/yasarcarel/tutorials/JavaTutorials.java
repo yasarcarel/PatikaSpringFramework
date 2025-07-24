0e7e95ca6907eda1916b00ec2fed12c508c6ad36
Yaşar Çarel <yasar.carel@gmail.com>
2025-07-24 14:32:00 +0300

    Hatırlatma

---
+++ src/main/java/com/yasarcarel/tutorials/JavaTutorials.java
@@ -0,0 +1,68 @@
+package com.yasarcarel.tutorials;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+        //Arrays.sort();
+        //this.
+        //super
+        //break ,continue ,return
+        //optional
+        //cast
+        //primitive type
+        //wrapper class
+        //Random
+        //Math
+        //java common rule
+        //syntax error , runtime error , logical error
+        //%s %d %f ==> printf();
+        //final
+        //static
+        //decoder ,encoder
+        //instanceof
+        //public ,private ,protected
+        //String
+        //StringBuilder
+        //StringBuffer
+        //StringTokenizer
+        //Synchronous , Asynchronous
+        //Enum
+        //Multithread
+
+        //S.O.L.I.D
+        //KISS
+        //DRY
+        //MONAD
+        //List(LinkedList ,ArrayList) ,Set ,Map
+        //Generic <>
+        //Iterator
+        //OOP
+        //Interface
+        //Abstract ,Inheritance what's the difference
+        //Clean Code
+        //Dummy Code
+
+        //UML - + __
+        //class
+        //POJO
+        //Bean
+        //Serileştirme
+        //INPUT OUTPUT IO
+        //JAR WAR
+        //Dağıtık programlama nedir
+        //server
+
+        //Constructor
+        //difference between exception and error
+
+        //Java 8: stream,map,filter,sort
+        //Optional
+        //Lambda Expression
+        //Method References
+
+        //D.P.
+
+
+
+    }
+}
