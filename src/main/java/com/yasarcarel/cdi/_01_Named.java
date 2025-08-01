a88ea7fc0d16429dbd6fe72e211ac1b571756af7
Yaşar Çarel <yasar.carel@gmail.com>
2025-08-01 14:02:51 +0300

    @Named

---
+++ src/main/java/com/yasarcarel/cdi/_01_Named.java
@@ -0,0 +1,24 @@
+package com.yasarcarel.cdi;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+//cdi ==> bean
+@Named(value = "namedtuto")
+@ApplicationScoped
+public class _01_Named {
+
+    private Long namedId;
+    private String namedData="merhabalar named cdi bean";
+}
