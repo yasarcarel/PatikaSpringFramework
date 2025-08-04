3661baa18d6e8795f92738040ad5671457e75763
Yaşar Çarel <yasar.carel@gmail.com>
2025-08-04 17:37:05 +0300

    Inversion Of Control(-) Dependency Injection(-)

---
+++ src/main/java/com/yasarcarel/iocsiz_disiz/Calisan.java
@@ -0,0 +1,20 @@
+package com.yasarcarel.iocsiz_disiz;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value= "cdisizCalisan")
+@ApplicationScoped
+public class Calisan {
+
+    private Patron patron;
+
+    //parametresiz constructor
+    public Calisan(){
+        patron=new Patron();
+    }
+
+    public String getData(String data){
+        return patron.surum(data);
+    }
+}
