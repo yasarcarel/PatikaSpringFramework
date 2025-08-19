572e9dc3ce0abcc14423ad829361b9c26f65446c
Yaşar Çarel <yasar.carel@gmail.com>
2025-08-19 10:34:30 +0300

    stereotype revise

--- src/main/java/com/yasarcarel/aop/stereotype/StereoTypeBean.java
+++ src/main/java/com/yasarcarel/aop/stereotype/StereoTypeBean.java
@@ -8,8 +8,8 @@ import javax.inject.Named;
 @BenimStereoType
 @Data @NoArgsConstructor @AllArgsConstructor
 public class StereoTypeBean {
-    private Long id;
-    private String name = "hamood";
+    private Integer id = 1453;
+    private String name = "Ahmet Yılmaz";
     private String trade;
 }
 //stereotype birden fazla annotation'i tek bir
